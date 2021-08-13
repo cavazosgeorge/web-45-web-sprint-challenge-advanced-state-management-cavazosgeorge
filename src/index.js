caca50a2d3024e5d6f159d1904b71c274d9729f1
George Cavazos <cavazosgeorge@yahoo.com>
2021-08-13 19:21:24 -0400

    Connect Redux to main App and Index Files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { reducer } from "./reducers";
+import { applyMiddleware } from "redux";
 
-const { worker } = require('./mocks/browser');
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const { worker } = require("./mocks/browser");
 worker.start();
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
